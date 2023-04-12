163b094637670177ae14bfa695bcf9a01870cf64
fenamattos <fernandamattosvieira@gmail.com>
2023-04-11 21:18:47 -0300

    compeonato instalado

--- routes/campeonatoRoutes.js
+++ routes/campeonatoRoutes.js
@@ -1,4 +1,10 @@
 const express = require('express');
+// Importamos o router do express
 const router = express.Router();
+const controller = require("../controllers/timeController");
 
+// Para usar ele, usamos router.METODO HTTP => Ex: router.get ou router.post
+// Além dele, precisamos informar 2 coisas: o nome da rota e 
+// qual fumção (controller) executar
+router.get("/consultar-equipe", controller.consultarTime);
 module.exports = router;
